4502a1ec7dca6108d98b28bf0a1f0d69e2c4e9fc
Maxim <podkmax@Onviss-MacBook-Pro.local>
2025-05-06 18:43:25 +0300

    second

--- IdeaProjects/MprojClientServ/src/Client.java
+++ IdeaProjects/MprojClientServ/src/Client.java
@@ -12,33 +12,6 @@ public class Client {
         DataCollector dataCollector = new DataCollector();
         Thread thread = new Thread(dataCollector);
         thread.start();
-      /*  ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        ObjectOutputStream outputStream = new ObjectOutputStream(byteArrayOutputStream);
-        int x = Integer.MAX_VALUE;
-        int y = 67;
-        long z = 656453354;
-        ArrayList<String> strings = new ArrayList<>();
-        strings.add("a");
-        strings.add("d");
-        strings.add("c");
-        String s = "d";
-        outputStream.writeInt(x);
-        outputStream.writeObject(s);
-      //  outputStream.writeInt(y);
-      //  outputStream.writeLong(z);
-         // outputStream.writeObject(s); // 8 8
-       // outputStream.writeObject(strings); // 70 72
-                                      //75 abcd 77 a
-        byte[] obj = byteArrayOutputStream.toByteArray();
 
-        System.out.println(obj.length);
-        М
-1143
-PLAIN
-778070078
-23Ночь
-34Вечер
-33Утро
-        */
     }
 }
